2b38ddf8f8495cdfdc1504f7fc82f08ef46918db
Yellin36 <yerin6860@gmail.com>
2023-01-02 21:06:59 +0900

    create project

---
+++ src/main/java/com/example/gitactionyerin/GitActionYerinApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitactionyerin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionYerinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActionYerinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitactionyerin/GitActionYerinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitactionyerin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionYerinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
